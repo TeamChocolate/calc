21727f61cd5d948ab709c2534a2082e9a36e152d
Martin O' Gorman <info@blahsoup.com>
2021-04-03 23:21:13 +0100

    final calculator

--- build.js
+++ build.js
@@ -473,3 +473,42 @@ if (project.page.hasClass('nutrition')) {
 $(".name").click(function() { 
 	$(this).find("i").toggleClass("fa-plus fa-check");
   });
+
+
+
+//   $(".select").click(function() { 
+	
+// 	$(function(){
+    
+// 		var iFrames = $('iframe');
+	  
+// 		function iResize() {
+		
+// 			for (var i = 0, j = iFrames.length; i < j; i++) {
+// 			  iFrames[i].style.height = iFrames[i].contentWindow.document.body.offsetHeight + 'px';}
+// 			}
+			
+// 			if ($.browser.safari || $.browser.opera) { 
+			
+// 			   iFrames.load(function(){
+// 				   setTimeout(iResize, 0);
+// 			   });
+			
+// 			   for (var i = 0, j = iFrames.length; i < j; i++) {
+// 					var iSource = iFrames[i].src;
+// 					iFrames[i].src = '';
+// 					iFrames[i].src = iSource;
+// 			   }
+			   
+// 			} else {
+// 			   iFrames.load(function() { 
+// 				   this.style.height = this.contentWindow.document.body.offsetHeight + 'px';
+// 			   });
+// 			}
+		
+// 		});
+
+
+//   });
+  
+  
